d86dd3eaa276348fa43346ce4bc8feb31f81a6cc
singhjashanpreetgsp@gmail.com <singhjashanpreetgsp@gmail.com>
2021-10-10 13:13:55 +0300

    Add project files.

---
+++ Software_Requirement_System_Jashan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Software_Requirement_System_Jashan.Startup))]
+namespace Software_Requirement_System_Jashan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
